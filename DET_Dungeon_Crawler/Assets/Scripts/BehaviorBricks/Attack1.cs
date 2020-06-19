d251d188a5239671fc3375766cffc88d6cdc7c04
wowo0520 <58699394+wowo0520@users.noreply.github.com>
2020-06-19 21:06:25 +0200

    boss attack1
    behaviortree

--- DET_Dungeon_Crawler/Assets/Scripts/BehaviorBricks/Attack1.cs
+++ DET_Dungeon_Crawler/Assets/Scripts/BehaviorBricks/Attack1.cs
@@ -8,18 +8,29 @@
     [Help("Führt die erste Attacke vom Boss aus")]
     public class Attack1 : BasePrimitiveAction
     {
-        // Define the input parameter "shootPoint".
-        [InParam("attackPoint")]
-        public Transform attackPoint;
+        // Define the input parameter "attack1Point".
+        [InParam("attack1Point")]
+        public Transform attack1Point;
  
-        // Define the input parameter "bullet" (the prefab to be cloned).
-        [InParam("bullet")]
-        public GameObject bullet;
+        // Define the input parameter "bossSlash" (the prefab to be cloned).
+        [InParam("bossSlash")]
+        public GameObject bossSlash;
  
         // Main class method, invoked by the execution engine.
         public override TaskStatus OnUpdate()
-        {             
-            return TaskStatus.COMPLETED; 
-        } // OnUpdate
- 
+        {
+        // Instantiate the bullet prefab.
+        GameObject newbossSlash = GameObject.Instantiate(
+                                    bossSlash, attack1Point.position,
+                                    attack1Point.rotation * bossSlash .transform.rotation
+                                ) as GameObject;
+        // Give it a velocity
+        if (bossSlash.GetComponent<Rigidbody>() == null)
+            // Safeguard test, although the rigid body should be provided by the
+            // prefab to set its weight.
+            bossSlash.AddComponent<Rigidbody>();
+        // The action is completed. We must inform the execution engine.
+        return TaskStatus.COMPLETED;
+
+       } // OnUpdate     
     } // class Attack1
