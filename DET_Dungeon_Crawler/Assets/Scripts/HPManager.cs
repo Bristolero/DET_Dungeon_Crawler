f2e6d0543cb5df7ac2fc49c91f426d0a3b53bdbc
wowo0520 <58699394+wowo0520@users.noreply.github.com>
2020-07-06 18:12:25 +0200

    hpSlider fertig

--- DET_Dungeon_Crawler/Assets/Scripts/HPManager.cs
+++ DET_Dungeon_Crawler/Assets/Scripts/HPManager.cs
@@ -15,14 +15,14 @@ public class HPManager : MonoBehaviour
     {
         hp = boss.GetComponent<Boss>().hp;
         hpTotal = boss.GetComponent<Boss>().hpTotal;
-        transform.Find("Attack_pos");
+        //transform.Find("Attack_pos");
     }
 
     // Update is called once per frame
     void Update()
     {
         //hpSlider.direction = Slider.Direction.RightToLeft;
-        hpPosition = boss.transform.Find("hpPos");
+        hpPosition.position = new Vector3(boss.transform.position.x-1.25f, boss.transform.position.y+1.5f, boss.transform.position.z);
         hpSlider.transform.position = hpPosition.position;
         hpTotal = boss.GetComponent<Boss>().hpTotal;
         hp = boss.GetComponent<Boss>().hp;
